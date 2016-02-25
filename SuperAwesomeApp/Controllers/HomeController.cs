1d16a59f9c348cd6ab6de69d6618e718a7e99c91
jsimpson <jsimpson@unionhomemortgage.com>
2016-02-25 12:56:21 -0500

    Added NumberName

---
+++ SuperAwesomeApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using SuperAwesomeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NumberName;
+
+namespace SuperAwesomeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Results(SuperAwesomeClass anything)
+        {
+            if (anything.Input != 0)
+                anything.Result = NumberName.NumberName.GetNumberName(anything.Input);
+            return View(anything);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SuperAwesomeApp/Models/SuperAwesomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperAwesomeApp.Models
+{
+    public class SuperAwesomeClass
+    {
+        public string Result { get; set; }
+        public int Input { get; set; }
+
+    }
+}
