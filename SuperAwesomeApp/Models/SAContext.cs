420509b61e7b39d8179c30a29e6d82c46fd2f384
Jim Cegelski <jcegelski@vloan.com>
2016-04-07 12:46:28 -0400

    got entity framework working

--- SuperAwesomeApp/Models/SAContext.cs
+++ SuperAwesomeApp/Models/SAContext.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace SuperAwesomeApp.Models
 {
     public class SAContext:DbContext
     {
         public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public SAContext()
+            : base("DefaultConnection")
+        { }
 
     }
 }
